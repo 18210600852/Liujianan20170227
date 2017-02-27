e47c8165d2f23ced4f97dac8e5d75604720c108c
家男 <363249149@qq.com>
2017-02-27 14:35:32 +0800

    Initial commit

---
+++ app/src/main/java/baway/com/liujianan20170227/Bean.java
@@ -0,0 +1,15 @@
+package baway.com.liujianan20170227;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 刘嘉男
+ * on 2017/2/27
+ * 描述:
+ */
+
+public class Bean {
+
+        public ArrayList<Data> data;
+
+}
---
+++ app/src/main/java/baway/com/liujianan20170227/Data.java
@@ -0,0 +1,15 @@
+package baway.com.liujianan20170227;
+
+/**
+ * Created by 刘嘉男
+ * on 2017/2/27
+ * 描述:
+ */
+
+public class Data {
+    public String img;
+    public String introduction;
+    public String userName;
+    public String userAge;
+    public String occupation;
+}
